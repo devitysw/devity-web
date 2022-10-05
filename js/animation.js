edb257af974d4638fa5475f30f59ff7b8ff4967f
Katka Marcincakova <katulik137@gmail.com>
2022-10-05 18:17:22 +0200

    preklikavanie medzi zariadeniami

--- js/animation.js
+++ js/animation.js
@@ -1,3 +1,15 @@
+var deviceIndex = 0;
 var deviceChanging = window.setInterval(function() {
-    $(".device-blob").toggle();
+    var devices = document.getElementsByClassName("devices");
+    $.each(devices, function(indexInArray, valueOfElement) {
+        if (!$(devices[indexInArray]).hasClass("faded")) {
+            $(devices[indexInArray]).addClass("faded");
+        }
+    });
+
+    $(devices[deviceIndex]).removeClass("faded");
+    deviceIndex++;
+    if (deviceIndex > 2) {
+        deviceIndex = 0;
+    }
 }, 2000);
