f5073485a4a57959457184af5256fd03d302c313
Katka Marcincakova <katulik137@gmail.com>
2022-10-06 10:52:28 +0200

    deleted logs

--- js/navbar.js
+++ js/navbar.js
@@ -1,10 +1,8 @@
 $(window).on("scroll touchmove", function() {
     if ($(document).scrollTop() > 100 && !$("#navbar").hasClass("bg-darkPurple")) {
-        console.log("1");
         $("#navbar").addClass("bg-darkPurple");
     }
     if ($(document).scrollTop() < 100 && $("#navbar").hasClass("bg-darkPurple")) {
-        console.log("2");
         $("#navbar").removeClass("bg-darkPurple");
     }
 })
