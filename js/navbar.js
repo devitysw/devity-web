d94b8e3eb09ec9f3fbb60da26c793a8b7a1952a8
Katka Marcincakova <katulik137@gmail.com>
2023-01-14 09:51:40 +0100

    kostol logo

--- js/navbar.js
+++
@@ -1,8 +0,0 @@
-$(window).on("scroll touchmove", function() {
-    if ($(document).scrollTop() > 100 && !$("#navbar").hasClass("bg-black")) {
-        $("#navbar").addClass("bg-black");
-    }
-    if ($(document).scrollTop() < 100 && $("#navbar").hasClass("bg-black")) {
-        $("#navbar").removeClass("bg-black");
-    }
-})
