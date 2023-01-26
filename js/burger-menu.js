ccf6a5f8d3d906027d0ebe1fd4dea7c696dcf737
Katka Marcincakova <katulik137@gmail.com>
2023-01-26 23:39:38 +0100

    mobile navbar smooth scroll

--- js/burger-menu.js
+++ js/burger-menu.js
@@ -1,8 +1,24 @@
 const btn = document.getElementById('menu-btn')
 const nav = document.getElementById('menu')
 
-btn.addEventListener('click', () => {
+function toggleMobileMenu() {
     btn.classList.toggle('open')
-    nav.classList.toggle('flex')
     nav.classList.toggle('hidden')
+
+    var x = 2 * $('#navbar').height();
+    $('#menu').css({ 'top': x + 'px' });
+}
+
+btn.addEventListener('click', () => {
+    toggleMobileMenu();
 })
+
+function scrollToSection(section) {
+    $('html').animate({
+        scrollTop: $(section).offset().top - 2 * ($("#navbar").height())
+    }, 100);
+
+    if (!$('#mobile').hasClass('block')) {
+        toggleMobileMenu();
+    }
+};
