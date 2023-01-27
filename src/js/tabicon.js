66b4b88a409d84c367f32525d1ad3549617ebef4
Katka Marcincakova <katulik137@gmail.com>
2023-01-27 22:32:43 +0100

    subory hodene do src

--- src/js/tabicon.js
+++ src/js/tabicon.js
@@ -2,8 +2,8 @@ const tabFavicon = document.getElementById("tabFavicon");
 const isDark = window.matchMedia("(prefers-color-scheme: dark)");
 
 const changeFavicon = () => {
-    if (isDark.matches) tabFavicon.href = "images/d-white.png";
-    else tabFavicon.href = "images/d.png";
+    if (isDark.matches) tabFavicon.href = "../images/d-white.png";
+    else tabFavicon.href = "../images/d.png";
 }
 
 changeFavicon();
