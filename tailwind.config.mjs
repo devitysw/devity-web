5101d618447d33d49c64f794d4ea0ee2e30706f6
KatkaKoreckova <katulik137@gmail.com>
2025-06-17 17:59:46 +0200

    logo carousel

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -20,6 +20,15 @@ export default {
                 lime: "#D8F52C",
                 purple: "#CBB0EF",
             },
+            animation: {
+                marquee: 'marquee 10s linear infinite',
+            },
+            keyframes: {
+                marquee: {
+                '0%': { transform: 'translateX(0)' },
+                '100%': { transform: 'translateX(-50%)' },
+                },
+            },
         },
     },
     plugins: [],
