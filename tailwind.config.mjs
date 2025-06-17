4df42440ad6b003d999b4f8b274e9d691ce5bdf8
KatkaKoreckova <katulik137@gmail.com>
2025-06-17 18:05:49 +0200

    fixed animation cut off

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -21,12 +21,12 @@ export default {
                 purple: "#CBB0EF",
             },
             animation: {
-                marquee: 'marquee 10s linear infinite',
+                marquee: 'marquee 30s linear infinite',
             },
             keyframes: {
                 marquee: {
                 '0%': { transform: 'translateX(0)' },
-                '100%': { transform: 'translateX(-50%)' },
+                '100%': { transform: 'translateX(-300%)' },
                 },
             },
         },
